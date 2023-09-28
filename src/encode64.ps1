9e4d0203b73c89cd2627b7c166b5b238ca77546a
Bruce Markham <219281+brucificus@users.noreply.github.com>
2023-09-28 19:58:42 -0400

    📝 Adds Documentation

--- src/encode64.ps1
+++ src/encode64.ps1
@@ -3,11 +3,37 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
+<#
+.SYNOPSIS
+    Outputs the given text or binary data as base64-encoded text.
+.PARAMETER Text
+    The text to encode.
+.PARAMETER Data
+    The binary data to encode.
+.PARAMETER Encoding
+    The encoding to use when converting the text to binary data. Defaults to UTF8.
+.OUTPUTS
+    System.String
+.EXAMPLE
+    encode64 "Hello World!"
+#>
 function encode64 {
     param(
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ParameterSetName="Text")]
         [ValidateNotNullOrEmpty()]
-        [string] $text
+        [string] $Text,
+
+        [Parameter(Mandatory=$false, Position=0, ValueFromPipeline=$true, ParameterSetName="Data")]
+        [ValidateNotNullOrEmpty()]
+        [byte[]] $Data,
+
+        [Parameter(Mandatory=$false, Position=1, ParameterSetName="Text")]
+        [ValidateNotNullOrEmpty()]
+        [Encoding] $Encoding = [System.Text.Encoding]::UTF8
     )
-    return [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($text))
+    if ($text) {
+        return [Convert]::ToBase64String($Encoding.GetBytes($Text))
+    } else {
+        return [Convert]::ToBase64String($Data)
+    }
 }
--- src/encodefile64.ps1
+++ src/encodefile64.ps1
@@ -3,14 +3,43 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 
+<#
+.SYNOPSIS
+    Converts the binary content of a file to base64 and writes the resulting text to a file.
+.PARAMETER LiteralPath
+    Path to the file to encode.
+.PARAMETER Encoding
+    The text encoding to use when writing the result to a file. Defaults to UTF8.
+.PARAMETER OutFile
+    Path to the file to write the encoded content to. If not specified, it will be the same as the input file with the extension ".txt" appended.
+.OUTPUTS
+    System.IO.FileInfo
+#>
 function encodefile64 {
     param(
         [Parameter(Mandatory=$true, Position=0)]
         [ValidateNotNullOrEmpty()]
-        [string] $file
+        [ValidateScript({ Test-Path $_ -PathType Leaf })]
+        [string] $LiteralPath,
+
+        [Parameter(Mandatory=$false, Position=1)]
+        [ValidateNotNullOrEmpty()]
+        [Encoding] $Encoding = [System.Text.Encoding]::UTF8,
+
+        [Parameter(Mandatory=$false, Position=2)]
+        [ValidateNotNullOrEmpty()]
+        [string] $OutFile = ($LiteralPath + ".txt")
     )
-    [string] $encoded = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($file))
-    [string] $encodedFile = "${file}.txt"
-    [System.IO.File]::WriteAllText($encodedFile, $encoded)
-    Write-Host "${file}'s content encoded in base64 and saved as ${encodedFile}"
+    Begin {
+        if (Test-Path $OutFile -ErrorAction SilentlyContinue) {
+            throw "The file already exists: ${OutFile}"
+        }
+    }
+    Process {
+        [string] $encoded = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($LiteralPath))
+        [System.IO.File]::WriteAllText($OutFile, $encoded, $Encoding)
+        return (Get-Item -LiteralPath $OutFile)
+    }
+    End {
+    }
 }
